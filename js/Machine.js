37e67ddf6a7784d53e72678af74907ebf9331f46
Krystian Konecki <krystian_konecki@wp.pl>
2019-07-02 15:04:08 +0200

    Adding refund and pay options

--- js/Machine.js
+++ js/Machine.js
@@ -7,13 +7,17 @@ class Machine {
         this.ticketItem = document.querySelector('.tickets__items');
         this.inputTickets = document.querySelector('.machine__input')
         this.clearAllButton = document.querySelector('[data-name = "clear"');
-        this.deleteTicketButton = document.querySelector('[data-name = "delete"');
+        this.deleteTicketButton = document.querySelector('[data-name = "delete"]');
         this.moneyInput = document.querySelector('.money__wrapper');
         this.moneyInputButton = document.querySelector('.money__button')
         this.addedMoneyInput = document.querySelector('.machine__money-added');
         this.ticketsValue = document.querySelector('.machine__ticket-value');
+        this.payButton = document.querySelector('[data-name = "pay"]')
+        this.refundButton = document.querySelector('[data-name = "refund"]')
+        this.showStatusButton = document.querySelector('.status__button--main');
 
         this.id = 0;
+        this.ifAdded = false
         this.chosenTickets = [];
         this.startMoney = [];
         this.NumberTab = [];
@@ -27,7 +31,7 @@ class Machine {
         const money = new Money();
         money.addMoney(this.startMoney, 10, 10, 10, 10, 10, 10);
         this.totalMoney = money.computeValue(this.startMoney);
-        
+
         this.addMoney();
         this.moneyAdded = [{
             name: "10gr",
@@ -55,7 +59,10 @@ class Machine {
             number: 0
         }];
         this.totalMoneyToPay = 0;
+        this.payMoney();
+        this.refundMoney();
         this.chartPie();
+        this.showStatus();
     }
 
     addTicket() {
@@ -164,6 +171,8 @@ class Machine {
                     this.inputTickets.removeChild(child)
                     child = this.inputTickets.lastElementChild;
                 }
+                this.totalMoneyToPay = 0;
+                this.ticketsValue.textContent = `Tickets value: ${this.totalMoneyToPay} zł`
             } else if (this.chosenTickets.length == 0) {
                 alert("No items to clear")
             }
@@ -172,18 +181,16 @@ class Machine {
     }
 
     chartPie() {
-        
+
         const PieChart = new ChartClass();
-        
+
         this.startMoney.forEach(element => {
             this.NumberTab.push(element.number);
         })
-        PieChart.drawChart(this.NumberTab[0],this.NumberTab[1],this.NumberTab[2],this.NumberTab[3],this.NumberTab[4],this.NumberTab[5])
-        console.log(this.NumberTab)
+
+        PieChart.drawChart(this.NumberTab[0], this.NumberTab[1], this.NumberTab[2], this.NumberTab[3], this.NumberTab[4], this.NumberTab[5])
+        //console.log(this.NumberTab)
     }
-       
-        
-    
 
     addMoney() {
         this.moneyInputButton.addEventListener('click', () => {
@@ -203,19 +210,131 @@ class Machine {
             console.log(this.moneyAdded)
             this.addedMoneyInput.textContent = `Added money: ${this.addedMoneyValue} zł`;
 
-            for(let i=0;i<this.moneyAdded.length;i++){
+            for (let i = 0; i < this.moneyAdded.length; i++) {
                 document.getElementById(`${this.moneyAdded[i].name}`).value = ""
             }
-            
+
             this.NumberTab = [];
             this.chartPie();
-            
+            this.ifAdded = true;
 
         })
 
 
     }
+
+    showStatus() {
+        this.showStatusButton.addEventListener('click', () => {
+            console.log(this.moneyAdded)
+            console.log(this.startMoney);
+        })
+    }
+
+    payMoney() {
+        this.payButton.addEventListener('click', () => {
+            if (this.ifAdded == true) {
+                if (this.chosenTickets.length != 0) {
+                    if (this.totalMoneyToPay > this.addedMoneyValue) {
+                        alert(`You have to pay ${this.totalMoneyToPay - this.addedMoneyValue} zł more`)
+                    } else if (this.totalMoneyToPay == this.addedMoneyValue) {
+                        alert("Thanks for using the servies :) ");
+                        this.ifAdded = false;
+                        this.moneyAdded.forEach((element) => {
+                            element.number = 0
+                        })
+                        this.addedMoneyValue = "0";
+                        this.addedMoneyInput.textContent = `Added money: ${this.addedMoneyValue} zł`;
+                        this.chartPie();
+                        this.chosenTickets = [];
+                        this.id = 0;
+                        var child = this.inputTickets.lastElementChild;
+                        while (child) {
+                            this.inputTickets.removeChild(child)
+                            child = this.inputTickets.lastElementChild;
+                        }
+                        this.totalMoneyToPay = 0;
+                        this.ticketsValue.textContent = `Tickets value: ${this.totalMoneyToPay} zł`
+
+                    } else if (this.totalMoneyToPay < this.addedMoneyValue) {
+                        let change = this.addedMoneyValue - this.totalMoneyToPay;
+                        alert(`Change: ${change}`);
+                        const arrayToCheck = this.startMoney.reverse()
+                        
+                        let i = 0;
+                        while (change != 0) {
+                            if (i >= 0 && i < arrayToCheck.length) {
+                                if (arrayToCheck[i].worth <= change) {
+                                    change -= arrayToCheck[i].worth;
+                                    arrayToCheck[i].number--;   
+                                } else {
+                                    i++;
+                                }
+                                
+                            }
+                        }
+
+                        this.ifAdded = false;
+                        this.moneyAdded.forEach((element) => {
+                            element.number = 0
+                        })
+                        this.addedMoneyValue = "0";
+                        this.addedMoneyInput.textContent = `Added money: ${this.addedMoneyValue} zł`;
+                        
+                        this.chosenTickets = [];
+                        this.id = 0;
+                        var child = this.inputTickets.lastElementChild;
+                        while (child) {
+                            this.inputTickets.removeChild(child)
+                            child = this.inputTickets.lastElementChild;
+                        }
+                        this.totalMoneyToPay = 0;
+                        this.ticketsValue.textContent = `Tickets value: ${this.totalMoneyToPay} zł`
+
+                        console.log(this.startMoney)
+                        console.log(arrayToCheck)
+
+                        /*this.startMoney.reverse();
+                        this.NumberTab = [];
+                        this.chartPie();*/
+
+                    }
+
+
+
+
+
+                    //this.ifAdded = false;
+                } else {
+                    alert("Choose ticekt first")
+                }
+            } else {
+                alert("Add money at first")
+            }
+        })
+    }
+
+    refundMoney() {
+        this.refundButton.addEventListener('click', () => {
+            if (this.ifAdded == true) {
+                this.startMoney.forEach((element, index) => {
+                    element.number -= this.moneyAdded[index].number
+                    this.moneyAdded[index].number = 0;
+                })
+                alert(`Refunded money: ${this.addedMoneyValue} zł`)
+                this.addedMoneyValue = "0";
+                this.addedMoneyInput.textContent = `Added money: ${this.addedMoneyValue} zł`;
+                this.ifAdded = false;
+                this.NumberTab = [];
+                this.chartPie();
+
+            } else {
+                alert("At first add money")
+            }
+        })
+    }
+
 }
+
 
 
 const ticketMachine = new Machine()
